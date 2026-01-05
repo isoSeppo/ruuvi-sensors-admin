b296763bd430dd5faab5b7fa2284ba9e03fbfa77
isoSeppo <seppo.isojarvi@hotmail.com>
2026-01-05 17:12:20 +0200

    Own bootstrap for admin tool

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -24,9 +24,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "b700fd601560e2f21e6828d5c68b2b51",
-"/": "b700fd601560e2f21e6828d5c68b2b51",
-"main.dart.js": "ea3858787cfa0e272998e3325c0321b4",
+"index.html": "e5e70d6d14b19f24305c593c962148e6",
+"/": "e5e70d6d14b19f24305c593c962148e6",
+"main.dart.js": "8b8e06614abe64a2821b791df27251fb",
 "manifest.json": "dcfeedd5a008a81a832801e31325a1ed",
 "version.json": "e5864914a964ff30313a74967d5c1cb5"};
 // The application shell files that are downloaded before a service worker can
